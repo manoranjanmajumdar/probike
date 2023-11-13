1de2759f64232d32fca6fa54922bd69e39fb284e
manoranjanmajumdar <143195446+manoranjanmajumdar@users.noreply.github.com>
2023-11-13 08:06:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ def hello_world():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8000)
+    app.run(debug=True,host='0.0.0.0',port=80)
 
